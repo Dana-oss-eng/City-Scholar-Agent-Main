3785bb9a3fc31bd203d69f4372769936f18254ed
Dana-oss-eng <2993108715@qq.com>
2026-06-17 21:49:29 +0800

    Delete llm directory

--- llm/__init__.py
+++
@@ -1,6 +0,0 @@
-"""LLM 抽象层：统一管理多家大模型服务商的调用接口。"""
-
-from llm.base import BaseLLMClient, LLMConfig
-from llm.factory import create_llm_clients, LLMProviders
-
-__all__ = ["BaseLLMClient", "LLMConfig", "create_llm_clients", "LLMProviders"]
